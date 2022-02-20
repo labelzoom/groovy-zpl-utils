4b9f576a7a78fab9df39ceb46e75e05a97603bf2
Rob Faust <robert.faust@labelzoom.net>
2022-02-19 22:40:21 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+![LabelZoom Logo](https://www.labelzoom.net/assets/base/images/LabelZoom_Logo_f.svg)
 # groovy-zpl-utils
 A collection of Groovy scripts to improve the experience of owning a Zebra printer
---
+++ printZpl.groovy
@@ -0,0 +1,6 @@
+def zpl = '^XA^FO20,20^A0N,48^FDgithub.com/labelzoom^FS^XZ' // TODO: Replace this with the ZPL you want to print
+def printerIpOrHostname = '192.168.0.44' // TODO: Replace this with the IP address or hostname of the printer
+try(def socket = new Socket(printerIpOrHostname, 9100); def pw = new PrintWriter(socket.getOutputStream()))
+{
+    pw.write(zpl)
+}
